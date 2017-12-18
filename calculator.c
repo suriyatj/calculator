a9c90c4721acdeb14a95ebba3db5fe39aa6b996d
Tilak Putta <putta.tilak@gmail.com>
2017-12-19 03:32:51 +0530

    basic calci added

--- calculator.c
+++ calculator.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-	/* code */
+	int a,b,c;
+	printf("Enter First number: ");
+	scanf("%d", &a);
+	printf("Enter Second number: ");
+	scanf("%d", &b);
+	c = a + b;
+	printf("Addition of given two numbers is %d\n", );
 	return 0;
 }
